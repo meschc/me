a89b4c7e749af13e3e36aeb07ee9da5884706b19
Mesch <.@example.com>
2025-04-17 20:16:38 +0300

    Update site

--- site-next/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "",
-  trailingSlash: true,
-};
-
-export default nextConfig;
--- site-next/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FileTree } from "@/components/FileTree";
-
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-white dark:bg-black">
-      <FileTree />
-    </div>
-  );
-}
